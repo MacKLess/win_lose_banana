5c90b37b5abf91336143ef114e4b3e7ec11aaf6c
L.D.MacKrell <ldmackrell@gmail.com>
2018-04-19 16:37:17 -0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'site#home'
+
+  resources :games, only: [:new, :create, :show, :destroy] do
+    resources :players, only: [:new, :create, :update]
+    resources :rounds, only: [:create, :show, :update]
+  end
 end
